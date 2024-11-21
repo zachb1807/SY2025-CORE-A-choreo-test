f66d0262eb9c878a0f25aa66a15f0c3c6bdbc0dd
jratke <jratke@cps.edu>
2024-11-21 11:30:29 -0600

    john/chore/swerve-update-couple-ratio

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -62,7 +62,7 @@ public class TunerConstants {
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
-    private static final double kCoupleRatio = 0;
+    private static final double kCoupleRatio = 3.125;
 
     private static final double kDriveGearRatio = 5.36;
     private static final double kSteerGearRatio = 18.75;
